a62762ead9ff4473c50721b1eb511c2d20c5ffb1
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2014-09-24 13:59:00 -0400

    Ordering of memory loading/unloading

--- src/vmod_ip2location.c
+++ src/vmod_ip2location.c
@@ -28,7 +28,7 @@ vmod_init_db(const struct vrt_ctx *ctx, struct vmod_priv *priv, const char *file
 	IP2Location *IP2LocationObj = IP2Location_open(filename);
 	if (IP2LocationObj == NULL)
 	{
-		printf("Please install the database in correct path.\n");
+		printf("Error locating database.\n");
 		return -1;
 	}
 
@@ -43,15 +43,18 @@ vmod_init_db(const struct vrt_ctx *ctx, struct vmod_priv *priv, const char *file
 VCL_STRING
 vmod_lookup_tz(const struct vrt_ctx *ctx, struct vmod_priv *priv, const char *ip)
 {
+	//Attach to the shared memory
 	IP2Location_open_mem(priv->priv, IP2LOCATION_SHARED_MEMORY);
+
+	//Lookup Record
 	IP2LocationRecord *record = IP2Location_get_all(priv->priv, ip);
+
+	//Make sure to detatch from the shared memory!
+	IP2Location_close(priv->priv);
 
 	if ( record != NULL ){
 		return record->timezone;
 	}
 
-	//Make sure to detatch from the shared memory!
-	IP2Location_close(priv->priv);
-
-	return "test";
+	return "-";
 }
